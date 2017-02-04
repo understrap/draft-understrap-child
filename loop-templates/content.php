d61ea4b812ed07a6f1125dbb20c6e653d6442c9a
Holger Könemann <office@holgerkoenemann.de>
2017-02-04 17:36:18 +0100

    adjusting headings

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -10,7 +10,7 @@
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 	<header class="entry-header">
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+		<?php the_title( sprintf( '<h2 class="entry-title h1"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
 		'</a></h2>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
