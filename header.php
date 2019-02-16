193c69381dee7a8d35abefe416ae454e96eb512e
KoenemannH <holger.koenemann@smartsteuer.de>
2019-02-16 10:54:11 +0100

    Update again

--- header.php
+++ header.php
@@ -33,46 +33,46 @@ $container = get_theme_mod( 'understrap_container_type' );
 		<a class="skip-link screen-reader-text sr-only" href="#content"><?php _e( 'Skip to content',
 		'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary">
+		<nav class="navbar navbar-toggleable-md navbar-inverse bg-primary">
 
-	<?php if ( 'container' == $container ) : ?>
-		<div class="container">
-	<?php endif; ?>
+		<?php if ( 'container' == $container ) : ?>
+			<div class="container">
+		<?php endif; ?>
 
-				<!-- Your site title as branding in the menu -->
-				<?php if ( ! has_custom_logo() ) { ?>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    				<span class="navbar-toggler-icon"></span>
+ 				 </button>
 
-					<?php if ( is_front_page() && is_home() ) : ?>
+					<!-- Your site title as branding in the menu -->
+					<?php if ( ! has_custom_logo() ) { ?>
 
-						<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php if ( is_front_page() && is_home() ) : ?>
 
-					<?php else : ?>
+							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
 
-						<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+						<?php else : ?>
 
-					<?php endif; ?>
+							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 
-				<?php } else {
-					the_custom_logo();
-				} ?><!-- end custom logo -->
+						<?php endif; ?>
 
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+
+					<?php } else {
+						the_custom_logo();
+					} ?><!-- end custom logo -->
 
 				<!-- The WordPress Menu goes here -->
-							<?php wp_nav_menu(
-								array(
-									'theme_location'  => 'primary',
-									'container_class' => 'collapse navbar-collapse',
-									'container_id'    => 'navbarNavDropdown',
-									'menu_class'      => 'navbar-nav ml-auto',
-									'fallback_cb'     => '',
-									'menu_id'         => 'main-menu',
-									'depth'           => 2,
-									'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-								)
-							); ?>
+				<?php wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'walker'          => new understrap_WP_Bootstrap_Navwalker(),
+					)
+				); ?>
 
 				<?php if ( is_active_sidebar( 'navbar-right' ) ) : ?><div class="navbar-right-widget"><?php dynamic_sidebar( 'navbar-right' ); ?></div><?php endif; ?>
 			<?php if ( 'container' == $container ) : ?>
